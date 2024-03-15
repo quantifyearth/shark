8ad76db6dee0f49440b6c03c1746c90335bc29b6
Patrick Ferris <patrick@sirref.org>
2024-03-15 15:51:12 +0000

    Fix macOS

--- vendor/obuilder/lib/sandbox.macos.ml
+++ vendor/obuilder/lib/sandbox.macos.ml
@@ -100,14 +100,22 @@ let run ~cancelled ?stdin:stdin ~log (t : t) config result_tmp =
   let proc_id = ref None in
   let proc =
     let stdin = Option.map (fun x -> `FD_move_safely x) stdin in
-    let pp f = Os.pp_cmd f ("", config.Config.argv) in
+    let pp f =
+      match config.argv with
+      | `Run argv -> Os.pp_cmd f ("", argv)
+      | `Terminal -> Os.pp_cmd f ("", ["terminal"])
+    in
     Os.sudo_result ~pp set_homedir >>= fun _ ->
     Os.sudo_result ~pp update_scoreboard >>= fun _ ->
     pre_build ~result_dir ~home_dir t >>= fun _ ->
     Os.pread @@ Macos.get_tmpdir ~user >>= fun tmpdir ->
     let tmpdir = List.hd (String.split_on_char '\n' tmpdir) in
     let env = ("TMPDIR", tmpdir) :: osenv in
-    let cmd = run_as ~env ~user ~cmd:config.Config.argv in
+    let cmd =
+      match config.Config.argv with
+      | `Run args -> run_as ~env ~user ~cmd:args
+      | `Terminal -> failwith "Terminal's are not supported on macOS use Linux"
+    in
     Os.ensure_dir config.Config.cwd;
     let pid, proc = Os.open_process ?stdin ~stdout ~stderr ~pp ~cwd:config.Config.cwd cmd in
     proc_id := Some pid;
@@ -130,6 +138,9 @@ let run ~cancelled ?stdin:stdin ~log (t : t) config result_tmp =
   copy_log >>= fun () ->
     if Lwt.is_sleeping cancelled then Lwt.return (r :> (unit, [`Msg of string | `Cancelled]) result)
     else Lwt_result.fail `Cancelled
+
+  let shell ~cancelled:_ ?stdin:_ ?unix_sock:_ _t _config _results_dir =
+    failwith "Shell's are not supported on macOS, use Linux"
 
 let create ~state_dir:_ c =
   Lwt.return {
