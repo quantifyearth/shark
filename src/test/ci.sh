3030776b0888998432d4f1da12f8b81f35d325fd
Patrick Ferris <patrick@sirref.org>
2024-03-20 16:24:43 +0000

    Fix rsync commands

--- src/test/ci.sh
+++ src/test/ci.sh
@@ -21,7 +21,7 @@ case "$1" in
         sudo mkdir /rsync
         sudo chown "$(whoami)" /rsync
 
-        opam exec -- dune exec -- shark md specs/shark.md --store=rsync:/rsync --rsync-mode copy
+        opam exec -- dune exec -- shark md specs/shark.md --store=rsync:/rsync
 
         sudo rm -rf /rsync
         ;;
