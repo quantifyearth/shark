f1e8e8c10d664c377bf0da8a09bbd4f5859046f6
Patrick Ferris <patrick@sirref.org>
2024-03-20 13:29:35 +0000

    Use zfs.img directly

--- src/test/ci.sh
+++ src/test/ci.sh
@@ -8,15 +8,12 @@ export OPAMYES=true
 case "$1" in
     zfs)
         dd if=/dev/zero of=/tmp/zfs.img bs=100M count=50
-        ZFS_LOOP=$(sudo losetup -f)
-        sudo losetup -P "$ZFS_LOOP" /tmp/zfs.img
         sudo /sbin/modprobe zfs
-        sudo zpool create obuilder-zfs "$ZFS_LOOP"
+        sudo zpool create obuilder-zfs /tmp/zfs.img
 
         opam exec -- dune exec -- shark md specs/shark.md
 
         sudo zpool destroy zfs
-        sudo losetup -d "$ZFS_LOOP"
         sudo rm -f /tmp/zfs.img
         ;;
 
