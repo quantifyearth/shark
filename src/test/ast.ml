5d2983e4c277c8ce7103f9b590a87d41c1cb2a95
Michael Dales <mwd24@cam.ac.uk>
2024-04-10 16:25:29 +0100

    Track cd to update workdir

--- src/test/ast.ml
+++ src/test/ast.ml
@@ -29,8 +29,25 @@ $ python3 else.py /data/something.txt
   let leaves = Shark.Commandgroup.children (List.nth groups 0) in
   Alcotest.(check int) "Single command expected" 2 (List.length leaves)
 
+let test_single_block_no_obvious_side_effects () =
+  let template_markdown =
+    {|
+```shark-run:containeralias
+$ python3 something.py
+```
+  |}
+  in
+  let test = Shark.Ast.of_sharkdown ~template_markdown in
+  let groups = Shark.Ast.to_list test in
+  Alcotest.(check int) "Single command group expected" 1 (List.length groups);
+  let leaves = Shark.Commandgroup.children (List.nth groups 0) in
+  Alcotest.(check int) "Single command expected" 1 (List.length leaves)
+
 let tests =
   [
     ("Test simple single command block", `Quick, test_single_block);
     ("Test simple milticommand block", `Quick, test_multicommand_block);
+    ( "Test simple block with non-obvious side effects",
+      `Quick,
+      test_single_block_no_obvious_side_effects );
   ]
