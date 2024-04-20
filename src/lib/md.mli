3d952d34687d120178668211cb1b66e9cd6a334f
Michael Dales <mwd24@cam.ac.uk>
2024-04-20 14:00:14 +0100

    Simple way to override env vars from command line

--- src/lib/md.mli
+++ src/lib/md.mli
@@ -27,6 +27,7 @@ val process_build_block :
   Cmarkit.Block.Code_block.t * Block.t
 
 val process_run_block :
+  ?env_override:(string * string) list ->
   fs:_ Eio.Path.t ->
   build_cache:Build_cache.t ->
   pool:unit Eio.Pool.t ->
