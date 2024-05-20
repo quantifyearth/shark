d4d677a5f927fbcfc8cceeb16cc85e448874043b
Michael Dales <mwd24@cam.ac.uk>
2024-05-20 17:48:46 +0100

    Change inputs to being a dictionary in frontmatter, so they can have short names

--- src/test/frontmatter.ml
+++ src/test/frontmatter.ml
@@ -8,27 +8,42 @@ let test_empty () =
   Alcotest.(check (result frontmatter msg))
     "Empty expected" (Ok Shark.Frontmatter.empty) res
 
-let test_inputs () =
+let test_parse_inputs () =
   let testcase =
     {|
 inputs:
-- /data/stuff
-- /data/other/
-- /data/file.txt
+  p1: /data/stuff
+  p2: /data/other/
+  p3: /data/file.txt
   |}
   in
   let res = Shark.Frontmatter.of_string testcase in
   let expected =
-    Shark.Frontmatter.v
-      [ ("inputs", [ "/data/stuff"; "/data/other/"; "/data/file.txt" ]) ]
+    Shark.Frontmatter.v []
       [
-        Fpath.v "/data/stuff"; Fpath.v "/data/other/"; Fpath.v "/data/file.txt";
+        ("p1", Fpath.v "/data/stuff");
+        ("p2", Fpath.v "/data/other/");
+        ("p3", Fpath.v "/data/file.txt");
       ]
   in
   Alcotest.(check (result frontmatter msg)) "Empty expected" (Ok expected) res
 
+let test_inputs_api () =
+  let testcase =
+    Shark.Frontmatter.v []
+      [
+        ("p1", Fpath.v "/data/stuff");
+        ("p2", Fpath.v "/data/other/");
+        ("p3", Fpath.v "/data/file.txt");
+      ]
+  in
+  let res = List.map Fpath.to_string (Shark.Frontmatter.inputs testcase) in
+  let expected = [ "/data/stuff"; "/data/other/"; "/data/file.txt" ] in
+  Alcotest.(check (list string)) "Empty expected" expected res
+
 let tests =
   [
     ("empty front matter", `Quick, test_empty);
-    ("simple input list", `Quick, test_inputs);
+    ("simple input list", `Quick, test_parse_inputs);
+    ("simple input api", `Quick, test_inputs_api);
   ]
